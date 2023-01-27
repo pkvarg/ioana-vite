5e45ed95d6331e5350de0962a03633cb18178f37
Peter Varga <pkvarg@yahoo.se>
2023-01-27 18:44:53 +0100

    footer mob

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,17 @@ const Footer = () => {
   return (
     <div className='bg-skin-color'>
       <div className='bg-hero-grey steep-footer md:pb-[50px] relative'>
-        <h1 className='text-[35px] text-dark-red ml-[40%] absolute top-[45%]'>
+        <h1 className='text-[35px] text-dark-red lg:ml-[40%] ml-[5px] absolute lg:top-[45%] top-[40%]'>
           Copyright &copy; {Date().substring(11, 15)} ioana-illustrations.eu
         </h1>
-        <h1 className='text-[35px] text-dark-red ml-[40%] absolute top-[60%]'>
-          info@ioana-illustrations.eu
-        </h1>
         <a
-          className='text-[35px] text-dark-red ml-[40%] absolute top-[75%]'
+          href='mailto:info@ioana-illustrations.eu'
+          className='text-[35px] text-dark-red lg:ml-[40%] ml-[15px] absolute lg:top-[60%]  top-[55%]'
+        >
+          info@ioana-illustrations.eu
+        </a>
+        <a
+          className='text-[35px] text-dark-red lg:ml-[40%] ml-[15px] absolute lg:top-[75%] top-[70%]'
           href='https://pictusweb.sk'
           target='_blank'
         >
